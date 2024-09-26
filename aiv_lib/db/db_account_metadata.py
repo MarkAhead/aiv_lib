9865f4639282217e90b2f5f1e8d6edcbdc7cc972
yadubhushan <yadubhushan2011@gmail.com>
2024-09-26 19:28:19 +0530

    Changed last published to next to publish

--- aiv_lib/db/db_account_metadata.py
+++ aiv_lib/db/db_account_metadata.py
@@ -1,5 +1,6 @@
 from .db_initialize import db
 import time
+import random
 
 collection_name = "account_metadata"
 
@@ -13,7 +14,7 @@ def fetch_last_post_timestamp(key):
         return account_data.get("last_posted")
 
 
-def update_current_timestamp(key, post_type):
+def update_publish_timestamp(key, post_type):
     timestamp = int(time.time() * 1000)  # Get the current time in milliseconds
     account = db.collection(collection_name).document(key)
     account_data = account.get().to_dict()
@@ -36,3 +37,21 @@ def fetch_specific_account_metadata(key):
         return None
     else:
         return account_data
+
+
+def update_next_post_time(key, post_type, min_delay_ms, max_delay_ms):
+    current_time_ms = int(time.time() * 1000)
+    next_post_time_ms = current_time_ms + random.randint(min_delay_ms, max_delay_ms)
+    
+    account = db.collection(collection_name).document(key)
+    account_data = account.get().to_dict()
+    
+    if account_data is None:
+        print(f"Account not found for key: {key}")
+        return None
+    else:
+        account_data["last_posted"] = current_time_ms
+        account_data["next_post_time"] = next_post_time_ms
+        account_data["last_post_type"] = post_type
+        account.set(account_data)
+        return next_post_time_ms
--- aiv_lib/db/db_initialize.py
+++ aiv_lib/db/db_initialize.py
@@ -1,4 +1,4 @@
 import hashlib
-from util_ConfigManager import get_firestore_client
+from ..util_ConfigManager import get_firestore_client
 # Get a Firestore client
 db = get_firestore_client()
