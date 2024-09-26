22d890a84c57d18bf187f0dc79149ccbc165be9e
yadubhushan <yadubhushan2011@gmail.com>
2024-09-26 16:40:47 +0530

    restructured code for direct pip install

--- aiv_lib_repo/build/lib/aiv_lib/cloud_gcp_functions.py
+++
@@ -1,9 +0,0 @@
-import functions_framework
-
-
-@functions_framework.http
-def my_http_function(request):
-    # Your code here
-
-    # Return an HTTP response
-    return "OK"
--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(
     name="aiv_lib",
     version="0.1",
     packages=find_packages(),
+    # Consider adding a description, author, and other metadata fields
+    # description="...",
+    # author="...",
+    # author_email="...",
+    # url="...",
+    
     install_requires=[
         'aiobotocore',
         'instagrapi',
@@ -20,4 +26,16 @@ setup(
         'torchvision',
         'whisperx @ git+https://github.com/m-bain/whisperx.git',
     ],
+    
+    # If there are any package data files, specify them
+    # package_data={
+    #     "": ["*.txt", "*.md"],
+    # },
+    
+    # If there are any entry points or scripts, define them
+    # entry_points={
+    #     "console_scripts": [
+    #         "my_script = my_package.module:function",
+    #     ],
+    # },
 )
